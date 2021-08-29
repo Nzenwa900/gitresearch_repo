e0fb14d50ce3142113b57616406c4bd51aa6a430
Nzenwa900 <cyrilnze@gmail.com>
2021-08-29 10:22:53 +0100

    javascript practice

---
+++ JS_practice.js
@@ -0,0 +1,47 @@
+// the hello world //
+console.log("Hello world");
+
+// the addition //
+console.log(1+1);
+
+// the multiplication //
+console.log(2*4);
+
+// the division // 
+console.log(5/2);
+
+// the subtraction //
+console.log(20-7);
+
+// this is a single line comment
+
+/* this is 
+a multiple line
+comment */
+
+// we have var 
+// we have let
+// we have const
+
+// using var //
+var firstName = "chidozie";
+console.log(firstName);
+firstName = "john";
+console.log(firstName);
+
+// using let //
+let a = 5
+console.log(a);
+a = 15;
+console.log(a);
+
+// using const //
+const PI = 3.142;
+console.log(PI);
+PI = 4.32;
+console.log(PI);
+
+
+
+
+
